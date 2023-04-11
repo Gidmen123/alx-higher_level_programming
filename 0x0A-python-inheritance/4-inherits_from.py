46f7c2e5d550b1437b1d854a69444db9ffb89cf7
Gideon Mensah <gomy1233@gmail.com>
2023-04-11 16:32:16 -0700

    Task A

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
         otherwise False.
     """
 
-    return isinstance(obj, type) and issubclass(obj, a_class)
+    return (issubclass(type(obj), a_class) and type(obj) is not a_class)
