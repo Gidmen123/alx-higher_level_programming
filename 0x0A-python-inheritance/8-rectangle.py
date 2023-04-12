12d4d8a8cb768aacbb19e8f3c211eaa41ceabc34
Gideon Mensah <gomy1233@gmail.com>
2023-04-11 17:11:23 -0700

    Task A

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = 0
         self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
         self.__width = width
         self.__height = height
